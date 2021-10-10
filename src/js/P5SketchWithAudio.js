ebb914be7852fe793b5316f52911536b8d525cd0
LABCAT <shane.watters.nz@gmail.com>
2021-10-10 14:51:30 +1300

    Further sketch experimentation and introducing vectors created by the drum pattern.

--- src/js/P5SketchWithAudio.js
+++ src/js/P5SketchWithAudio.js
@@ -28,15 +28,17 @@ const P5SketchWithAudio = () => {
 
         p.colours = ["#FFC618", "#F42539", "#4178F4", "#FE84FE", "#FF8119", "#56AC51", "#9819FA"];
 
-        p.grid = [];
+        p.colours2 = ['#ff0000','#ffa500','#ffff00','#008000','#0000ff','#4b0082','#ee82ee'];
 
         p.loadMidi = () => {
             Midi.fromUrl(midi).then(
                 function(result) {
                     const noteSet1 = result.tracks[3].notes, // Maelstrom 3 - Coins
-                        noteSet2 = result.tracks[4].notes; // Maelstrom 4 - Twerpoff
+                        noteSet2 = result.tracks[4].notes, // Maelstrom 4 - Twerpoff
+                        noteSet3 = result.tracks[0].notes; // Redrum 1
                     p.scheduleCueSet(noteSet1, 'executeCueSet1');
                     p.scheduleCueSet(noteSet2, 'executeCueSet2');
+                    p.scheduleCueSet(noteSet3, 'executeCueSet3', true);
                     p.audioLoaded = true;
                     document.getElementById("loader").classList.add("loading--complete");
                     document.getElementById("play-icon").classList.remove("fade-out");
@@ -45,13 +47,13 @@ const P5SketchWithAudio = () => {
             
         }
 
-        p.scheduleCueSet = (noteSet, callbackName)  => {
+        p.scheduleCueSet = (noteSet, callbackName, poly = false)  => {
             let lastTicks = -1, 
                 currentCue = 1;
             for (let i = 0; i < noteSet.length; i++) {
                 const note = noteSet[i],
                     { ticks, time } = note;
-                if(ticks !== lastTicks){
+                if(ticks !== lastTicks || poly){
                     note.currentCue = currentCue;
                     p.song.addCue(time, p[callbackName], note);
                     lastTicks = ticks;
@@ -73,11 +75,10 @@ const P5SketchWithAudio = () => {
 
         p.draw = () => {
             if(p.audioLoaded && p.song.isPlaying()){
-
             }
         }
 
-        p.drawCrystal = (x, y, delayAmount, width, height = width, shape = 'rect') => {
+        p.drawSparkle = (x, y, delayAmount, width, height = width, shape = 'rect') => {
             const colour = p.color(p.random(p.colours));
             colour.setAlpha(10);
             let t = p.random(88888),
@@ -104,6 +105,13 @@ const P5SketchWithAudio = () => {
                         p.translate(x, y);
                         p.rotate(item.rotation);
                         p.stroke(item.colour);
+                        p.strokeWeight(1);
+                        if(shape === 'rect'){
+                            p.strokeWeight(p.random(1, 2));
+                        }
+                        if(shape === 'equilateral'){
+                            p.strokeWeight(3);
+                        }
                         p.noFill();
                         p[shape](0, 0, item.width, item.height);
                         t += 0.001;
@@ -122,15 +130,15 @@ const P5SketchWithAudio = () => {
                 delay = parseInt(duration * 1000) / 1000,
                 w = currentCue === 16 ? p.width / 2 : p.width / 7  * p.random(0.2, 2),
                 h = currentCue === 16 ? p.height / 2 : p.height / 4  * p.random(0.2, 2),
-                shape = currentCue === 16 ? 'equilateral' : 'rect';
+                shape = currentCue === 16 ? 'equilateral' :'rect';
                 
                 
             if(currentCue === 16) {
                 p.clear();
                 p.background(0);
-                p.drawCrystal(x, y, delay, w / 4, h / 4, 'hexagon');
+                p.drawSparkle(x, y, delay, w / 4, h / 4, 'hexagon');
             }
-            p.drawCrystal(x, y, delay, w, h, shape);
+            p.drawSparkle(x, y, delay, w, h, shape);
         }
 
         p.executeCueSet2 = (note) => {
@@ -141,25 +149,47 @@ const P5SketchWithAudio = () => {
                 w = p.width / 7  * p.random(0.5, 2),
                 h = p.height / 4  * p.random(0.5, 2),
                 shape = p.random(['ellipse', 'rect']);
-            p.drawCrystal(x, y, delay, w, h, shape);
+            p.drawSparkle(x, y, delay, w, h, shape);
+        }
+
+        p.executeCueSet3 = (note) => {
+            // p.particles.push(new Particle(p, p.random(p.width), p.random(p.height)));
+            const colour = p.color(p.random(p.colours)),
+                x1 = p.random(0, p.width),
+                y1 = p.random(0, p.height),
+                x2 = p.random(0, p.width),
+                y2 = p.random(0, p.height),
+                x3 = p.random(0, p.width),
+                y3 = p.random(0, p.height);
+            colour.setAlpha(90);
+            p.noStroke();
+            p.fill(colour);
+            p.translate(x1, y1);
+            p.scale(0.1);
+            p.beginShape();
+            p.vertex(x1,y1);
+            p.vertex(x2,y2);
+            p.vertex(x3,y3);
+            p.vertex(x1,y1);
+            p.endShape();
         }
 
         /*
-       * function to draw an equilateral triangle with a set width
-       * based on x, y co-oridinates that are the center of the triangle
-       * @param {Number} x        - x-coordinate that is at the center of triangle
-       * @param {Number} y      	- y-coordinate that is at the center of triangle
-       * @param {Number} width    - radius of the hexagon
-       */
-      p.equilateral = (x, y, width) => {
-        const x1 = x - width / 2;
-        const y1 = y + width / 2;
-        const x2 = x;
-        const y2 = y - width / 2;
-        const x3 = x + width / 2;
-        const y3 = y + width / 2;
-        p.triangle(x1, y1, x2, y2, x3, y3);
-      };
+        * function to draw an equilateral triangle with a set width
+        * based on x, y co-oridinates that are the center of the triangle
+        * @param {Number} x        - x-coordinate that is at the center of triangle
+        * @param {Number} y      	- y-coordinate that is at the center of triangle
+        * @param {Number} width    - radius of the hexagon
+        */
+        p.equilateral = (x, y, width) => {
+            const x1 = x - width / 2;
+            const y1 = y + width / 2;
+            const x2 = x;
+            const y2 = y - width / 2;
+            const x3 = x + width / 2;
+            const y3 = y + width / 2;
+            p.triangle(x1, y1, x2, y2, x3, y3);
+        };
 
       /*
        * function to draw a hexagon shape
@@ -168,38 +198,18 @@ const P5SketchWithAudio = () => {
        * @param {Number} y      - y-coordinate of the hexagon
        * @param {Number} radius   - radius of the hexagon
        */
-      p.hexagon = (x, y, radius) => {
-        radius = radius / 2;
-        p.angleMode(p.RADIANS);
-        const angle = p.TWO_PI / 6;
-        p.beginShape();
-        for (var a = p.TWO_PI / 12; a < p.TWO_PI + p.TWO_PI / 12; a += angle) {
-          let sx = x + p.cos(a) * radius;
-          let sy = y + p.sin(a) * radius;
-          p.vertex(sx, sy);
+        p.hexagon = (x, y, radius) => {
+            radius = radius / 2;
+            p.angleMode(p.RADIANS);
+            const angle = p.TWO_PI / 6;
+            p.beginShape();
+            for (var a = p.TWO_PI / 12; a < p.TWO_PI + p.TWO_PI / 12; a += angle) {
+                let sx = x + p.cos(a) * radius;
+                let sy = y + p.sin(a) * radius;
+                p.vertex(sx, sy);
+            }
+            p.endShape(p.CLOSE);
         }
-        p.endShape(p.CLOSE);
-      }
-
-      /*
-       * function to draw a octagon shape
-       * adapted from: https://p5js.org/examples/form-regular-polygon.html
-       * @param {Number} x        - x-coordinate of the octagon
-       * @param {Number} y      - y-coordinate of the octagon
-       * @param {Number} radius   - radius of the octagon
-       */
-      p.octagon = (x, y, radius) => {
-        radius = radius / 2;
-        p.angleMode(p.RADIANS);
-        const angle = p.TWO_PI / 8;
-        p.beginShape();
-        for (var a = p.TWO_PI / 16; a < p.TWO_PI + p.TWO_PI / 16; a += angle) {
-          let sx = x + p.cos(a) * radius;
-          let sy = y + p.sin(a) * radius;
-          p.vertex(sx, sy);
-        }
-        p.endShape(p.CLOSE);
-      }
 
         p.mousePressed = () => {
             if(p.audioLoaded){
@@ -229,7 +239,7 @@ const P5SketchWithAudio = () => {
                     "\n",
                     "Animation By: https://github.com/LABCAT/",
                     "\n",
-                    "Coding Inspiration: https://openprocessing.org/sketch/1034762 and https://editor.p5js.org/Nostrada/sketches/fEU5hRoIq"
+                    "Coding Inspiration: https://openprocessing.org/sketch/1034762"
                 );
                 p.song.stop();
             }
@@ -243,19 +253,6 @@ const P5SketchWithAudio = () => {
                 console.log('Random colour set created...')
                 p.colours = p.randomColor({luminosity: 'bright', count: 7});
             }
-            const width = p.width / 7,
-                height = p.height / 4;
-            for (let x = 0; x < 7; x++) {
-                for (let y = 0; y < 4; y++) {
-                    p.grid.push(
-                        {
-                            x: x * width + width / 2,
-                            y: y * height + height / 2
-                        }
-                    )
-                }
-            }
-            p.grid = ShuffleArray(p.grid);
         }
 
         p.updateCanvasDimensions = () => {
